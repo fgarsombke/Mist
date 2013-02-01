a3847090c11dbc5279142683a66d5d44afff290a
makilian <maakilian@gmail.com>
2013-02-01 15:31:30 -0600

    begun adding session support

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import web
+import sys
 from api.user import user
 from api.user import list
 from api.device import device
@@ -7,6 +8,7 @@ from api.station import station
 from api.zone import zone
 from api.index import index
 from api.schedule import schedule
+sys.path.append('/Users/makilian/Mist/db')
 
 #web.config.debug = False
 
@@ -22,7 +24,18 @@ urls = (
 	'/schedule', 'schedule'
 )
 
+config = open("/Users/makilian/Mist/local.config")
+host = config.readline().rstrip()
+user = config.readline().rstrip()
+password = config.readline().rstrip()
+database = config.readline().rstrip()
+
+
 app = web.application(urls, globals())
+
+db = web.database(dbn='mysql', db=database, user=user, pw=password)
+store = web.session.DBStore(db, 'sessions')
+session = web.session.Session(app, store, initializer={'count': 0})
 
 if __name__ == '__main__':
 	app.run()  
