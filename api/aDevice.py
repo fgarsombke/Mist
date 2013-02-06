84abcb68535539ca16181a66b753a8fe079058d0
makilian <maakilian@gmail.com>
2013-02-06 13:24:35 -0600

    Modified api & web structure to create a business logic layer for the website to decouple it from the API.

---
+++ api/aDevice.py
@@ -0,0 +1,81 @@
+import web
+import sys
+import hashlib
+import uuid
+import base64
+import MySQLdb
+sys.path.append('../db/')
+from db import DBConfig
+
+def insertDeviceInDatabase(productId, userId, latitude, longitude, stationID):
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()	
+	cursor = db.cursor()
+	cursor.execute("INSERT INTO devices (productID, userID, latitude, longitude, climateStationID) VALUES (%s, %s, %s, %s, %s)", (productId, userId, latitude, longitude, stationID))
+	results = cursor.lastrowid
+	db.commit()
+	return results
+
+def getAllDevices():
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()	
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM devices")
+	results = cursor.fetchall()
+	return results	
+
+def getDevicesForUser(userid):
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()	
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM devices WHERE devices.userID = (%s)", userid)
+	results = cursor.fetchall()
+	return results	
+
+def getDevice(deviceid):
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()	
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM devices WHERE devices.productID = (%s)", deviceid)
+	results = cursor.fetchone()
+	return results	
+
+def findNearestClimateStation(latitude, longitude):
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()
+	cursor = db.cursor()
+	cursor.execute("SELECT *, 3956 * 2 * ASIN(SQRT(POWER(SIN((%s - latitude) *  pi()/180 / 2), 2) + COS(%s * pi()/180) * COS(%s * pi()/180) * POWER(SIN((%s - longitude) * pi()/180 / 2), 2) )) as distance FROM climateStations ORDER BY distance limit 1", (latitude, latitude, latitude, longitude)) # THIS FINDS THE NEAREST WEATHER STATION
+	nearestStation = cursor.fetchone()
+	return nearestStation[0]
+
+
+class aDevice:
+	#GET API FOR DEVICE
+	#RRETURN one device: expected parameter: deviceid (long)
+	#RETURN all devices: expetected parameter: none?
+	def GET(self):
+		device_data = web.input()	
+		devices = 0		
+	
+		if device_data.deviceid:
+			devices = getDevice(device_data.deviceid)
+		else:
+			devices = getAllDevices()
+	
+		#TODO: JSONize output
+		return devices
+
+	#POST API FOR DEVICE CREATION
+	#expeceted parameters: userid, latitude, longitude, wifi network, wifi password, stationID 
+	#return DEVICE ID?  or actuallt maybe this is supplied by the device it self???
+	def POST(self):
+		device_data = web.input()	
+		userId = 0	
+		#verify parameters
+		if device_data:
+			#find nearest climate station!
+			assignedStationID = findNearestClimateStation(device_data.latitude, device_data.longitude)
+			userId = insertDeviceInDatabase(device_data.productid, device_data.userid, device_data.latitude, device_data.longitude, assignedStationID) #SQL
+		#TODO: JSONize output
+		return userId
+		
---
+++ api/aStation.py
@@ -0,0 +1,60 @@
+import web
+import sys
+import hashlib
+import uuid
+import base64
+import MySQLdb
+sys.path.append('../db')
+from db import DBConfig
+
+def insertStationInDatabase(stationID, desc, latitude, longitude):
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()
+	cursor = db.cursor()
+	cursor.execute("INSERT INTO climateStations (climateStationID, description, latitude, longitude) VALUES (%s, %s, %s, %s)", (stationID, desc, latitude, longitude))
+	results = cursor.lastrowid
+	db.commit()
+	return results
+
+def getAllStations():
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM climateStations")
+	results = cursor.fetchall()
+	return results
+
+def getStation(stationID):
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM climateStations WHERE climateStations.climateStationID = (%s)", stationID)
+	results = cursor.fetchone()
+	return results	
+
+class aStation:
+	#GET API FOR STATION
+	#reutrn one station: expected parameter: stationid (long)
+	#return all stations: expected parameter: null
+	def GET(self):
+		station_data = web.input()
+	
+		#parameters
+		if station_data.stationid:	
+			station = getStation(station_data.staionid) # SQL
+			return station 
+		else:
+			stations = getAllStations()
+			return stations
+
+	#POST API FOR STATION CREATION
+	#expeceted parameters: stationid, description, latitude, longitude
+	#return STATION ID? 
+	def POST(self):
+		station_data = web.input()	
+		
+		#verify parameters
+
+		#if verified, put in database
+		stationId = insertStationInDatabase(station_data.stationid, station_data.description, station_data.latitude, station_data.longitude) #SQL
+		return stationId
