9837682ebbb6730730997d87f8bd146722c9968b
makilian <maakilian@gmail.com>
2013-02-13 13:48:09 -0600

    Removed webm/device post request and associated methods

--- webm/device.py
+++ webm/device.py
@@ -10,28 +10,10 @@ import urllib2
 import urllib
 import json
 
-def insertDeviceInDatabase(productId, userId, latitude, longitude, stationID):
-    conf = DBConfig.DBConfig()
-    db = conf.connectToLocalConfigDatabase()    
-    cursor = db.cursor()
-    cursor.execute("INSERT INTO devices (productID, userID, latitude, longitude, climateStationID) VALUES (%s, %s, %s, %s, %s)", (productId, userId, latitude, longitude, stationID))
-    results = cursor.lastrowid
-    db.commit()
-    return results
-
-def findNearestClimateStation(latitude, longitude):
-    conf = DBConfig.DBConfig()
-    db = conf.connectToLocalConfigDatabase()
-    cursor = db.cursor()
-    cursor.execute("SELECT *, 3956 * 2 * ASIN(SQRT(POWER(SIN((%s - latitude) *  pi()/180 / 2), 2) + COS(%s * pi()/180) * COS(%s * pi()/180) * POWER(SIN((%s - longitude) * pi()/180 / 2), 2) )) as distance FROM climateStations ORDER BY distance limit 1", (latitude, latitude, latitude, longitude)) # THIS FINDS THE NEAREST WEATHER STATION
-    nearestStation = cursor.fetchone()
-    return nearestStation[0]
-
 class addDevice:
     def GET(self):
         render = web.template.render('templates/', base='layout')
         return render.addDevice()
-
 
     def POST(self):
         render = web.template.render('templates/', base='layout')
@@ -89,14 +71,5 @@ class device:
     #expeceted parameters: userid, latitude, longitude, wifi network, wifi password, stationID 
     #return DEVICE ID?  or actuallt maybe this is supplied by the device it self???
     def POST(self):
-        device_data = web.input()   
         render = web.template.render('templates/')
-        
-        #verify parameters
-
-        #find nearest climate station!
-        assignedStationID = findNearestClimateStation(device_data.latitude, device_data.longitude)
-
-        #if verified, put in database
-        userId = insertDeviceInDatabase(device_data.productid, device_data.userid, device_data.latitude, device_data.longitude, assignedStationID) #SQL
-        return render.user(userId)
+        return render.device()
