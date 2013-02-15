da67f78d1e033172b6fc03505c94709dc9c4e641
Michael Graczyk <michael@mgraczyk.com>
2013-02-15 05:59:23 -0600

    Changed neighbor data storage to a c_matrix for performance and ease of coding reasons.

--- simulators/LawnSim/inc/NeighborData.h
+++ simulators/LawnSim/inc/NeighborData.h
@@ -10,46 +10,48 @@ class NeighborData {
 public:
    typedef T value_t;
    typedef bm::c_matrix<value_t, 3, 3> data_t;
-   typedef data_t iterator;
-   typedef data_t const_iterator;
+   typedef value_t * iterator;
+   typedef const value_t * const_iterator;
 
    // Allow NeighborData to be assigned from a constant
    NeighborData& operator=(value_t value) {
-      for (int i = 0; i < 9; ++i) {
-         data_[i] = value;
+      for (value_t &val : *this) {
+         val = value;
       }
 
       return *this;
    }
 
-   data_t &data() { return data_; }
+   const data_t &data() const { return data_; }
 
-   iterator begin() { return data_.data().begin(); }
-   iterator end() { return data_ + 9; }
+   iterator begin() { return &data_.data()[0]; }
+   iterator end() { return &data_.data()[0] + 9; }
 
-   const_iterator begin() const { return data_; }
-   const_iterator end() const { return data_ + 9; }
+   const_iterator begin() const { return &data_.data()[0]; }
+   const_iterator end() const { return &data_.data()[0] + 9; }
 
-   value_t Right() const { return data_(1,2); }
-   value_t TopRight() const { return data_(0,2); }
-   value_t Top() const { return data_(0,1); }
-   value_t TopLeft() const { return data_(0,0); }
-   value_t Left() const { return data_(1,0); }
-   value_t BottomLeft() const { return data_(2,0); }
-   value_t Bottom() const { return data_(2,1); }
-   value_t BottomRight() const { return data_(2,2); }
-   value_t Self() const { return data_(1,1); }
+   const value_t &Right() const { return data_(1,2); }
+   const value_t &TopRight() const { return data_(0,2); }
+   const value_t &Top() const { return data_(0,1); }
+   const value_t &TopLeft() const { return data_(0,0); }
+   const value_t &Left() const { return data_(1,0); }
+   const value_t &BottomLeft() const { return data_(2,0); }
+   const value_t &Bottom() const { return data_(2,1); }
+   const value_t &BottomRight() const { return data_(2,2); }
+   const value_t &Center() const { return data_(1,1); }
 
-   void SetRight(value_t value) { data_(1,2) = value; }
-   void SetTopRight(value_t value) { data_[1] = value; }
-   void SetTop(value_t value) { data_[2] = value; }
-   void SetTopLeft(value_t value) { data_[3] = value; }
-   void SetLeft(value_t value) { data_[4] = value; }
-   void SetBottomLeft(value_t value) { data_[5] = value; }
-   void SetBottom(value_t value) { data_[6] = value; }
-   void SetBottomRight(value_t value) { data_[7] = value; }
-   void SetSelf(value_t value) { data_[8] = value; }
-
+   value_t &Right()  { return data_(1,2); }
+   value_t &TopRight()  { return data_(0,2); }
+   value_t &Top()  { return data_(0,1); }
+   value_t &TopLeft()  { return data_(0,0); }
+   value_t &Left()  { return data_(1,0); }
+   value_t &BottomLeft()  { return data_(2,0); }
+   value_t &Bottom()  { return data_(2,1); }
+   value_t &BottomRight()  { return data_(2,2); }
+   value_t &Center()  { return data_(1,1); }
+   
+protected:
+   data_t &data_mutable() { return data_; }
 
 private:
    // Drift constants starting at the right going counter clockwise
