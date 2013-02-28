7dfd31e7b5e60a3cb398cd36a12095ec8753ac88
makilian <maakilian@gmail.com>
2013-02-28 17:49:25 -0600

    minor stuff

--- inc/WeatherDataSource.h
+++ inc/WeatherDataSource.h
@@ -11,9 +11,11 @@ public:
    
    WeatherData GetData(GeoLocale locale, pt::time_period period) {
       // TODO: Implement
+
       //Create URL request to weather API - parameters are lat, long & time.
       //the API initially will just find the closest weather station and get
       //the 'WeatherData' closest to the requested time
+      
       return WeatherData();
    }
 
