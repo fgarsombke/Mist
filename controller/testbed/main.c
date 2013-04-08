152b24376f17c7a24f5752c241336d3225d88ef7
Nathan Thai <nathanqthai@gmail.com>
2013-04-08 07:14:07 -0500

    Fetches schedule properly

--- controller/testbed/main.c
+++ controller/testbed/main.c
@@ -8,7 +8,6 @@
 #include "driverlib/sysctl.h"
 #include "drivers/rit128x96x4.h"
 #include "wifly.h"
-#include "rgb_led.h"
 #include "stdlib.h"
 #include "string.h"
 #include "stdio.h"
@@ -19,15 +18,11 @@ void __error__(char *pcFilename, unsigned long ulLine){}
 
 int main(void){
     volatile unsigned long time;
-    char resp[2048];
     RIT128x96x4Init(1000000);
     RIT128x96x4Clear();
     WiFly_Init();
-    time = WiFly_Time();
     while(1) {
-        int x = 0;
-        WiFly_Open(resp, 2048);
-        x++;
+        WiFly_Open();
     }
 }
 
