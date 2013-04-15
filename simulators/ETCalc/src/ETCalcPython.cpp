e463dffb972b6ccce0b3424e9524e85ae05c5d9d
Michael Graczyk <michael@mgraczyk.com>
2013-04-15 02:10:07 -0500

    Added ETCalc project files

--- simulators/ETCalc/src/ETCalcPython.cpp
+++ simulators/ETCalc/src/ETCalcPython.cpp
@@ -2,8 +2,10 @@
 
 using namespace Mist::ETCalc;
 
-BOOST_PYTHON_MODULE(ETCalc)
+BOOST_PYTHON_MODULE(libETCalc)
 {
     class_<ETCalc>("ETCalc", init<double, double>())
 		 .def("CalculateET_o", &ETCalc::CalculateET_o);
+
+	 //class_<DataSetOptional>("DataSetOptional");
 }
