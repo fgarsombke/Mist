2ade6fa8380aea02ff9fc3f6f78f77ad54a8c0aa
Nathan Thai <nathanqthai@gmail.com>
2013-04-15 05:35:22 -0500

    Randomly crashes, hard faults when parsing
    Hard faults on the first endTime of Zone 3. I don't know why.

--- embedded/hardware/src/schedule.c
+++ embedded/hardware/src/schedule.c
@@ -1,24 +1,25 @@
 #include "inc/hw_types.h"
 
-//#include "drivers/rit128x96x4.h"
-
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 
+#include "fifo.h"
+#include "json.h"
 #include "schedule.h"
 #include "wifly.h"
 
-const char* ZONE_LABEL = "z";
-const char* ZONE_NUM_LABEL = "zoneNumber";
-const char* TIMES_ARRAY_LABEL = "times";
-const char* START_LABEL = "startTime";
-const char* END_LABEL = "endTime";
+// for sorting
+typedef int (*compfn)(const void*, const void*);
+
+// Constants used for parsing
 const char* JSON_START = "\r\n\r\n";
 const char* JSON_END = "*CLOS*";
 
-schedule_entry_t day_schedule[40];
-
+// Global variable for storing a week of schedules
+#define SCHED_SIZE 40
+schedule_entry_t schedule[SCHED_SIZE];
+size_t schedule_idx = 0;
 
 // Extracts the JSON from HTTP data and returns it as a NULL terminated string.
 // Does not create a new string because of RAM constraints.
@@ -29,9 +30,33 @@ void scheduleExtract(char *http_data) {
     while((*json_start == '\r') || (*json_start == '\n')) json_start++; // Consume until JSON
     *json_end = NULL;
 
-    http_data = strcpy(http_data, json_start); // move to the front of the string
+    http_data = strcpy(http_data, json_start);   
+}
+// Parses a JSON schedule into the global variable
+// schedule and sorts it
+void scheduleParse(char *json) {
+    JSON_Parse(json);
 }
 
-// Extracts the zones and times from a JSON formatted string
-void scheduleParse(char *json){
+// schedule_entry_t
+int compare(schedule_entry_t* elem1, schedule_entry_t* elem2){
+   if (elem1->start_time < elem2->start_time)
+      return -1;
+
+   else if (elem1->start_time > elem2->start_time)
+      return 1;
+
+   else
+      return 0;
 }
+
+// Sorts the schedule
+void scheduleSort(void) {
+   qsort((void *) &schedule,    // Beginning address of array
+   SCHED_SIZE,                  // Number of elements in array
+   sizeof(schedule_entry_t),    // Size of each element
+   (compfn)compare );           // Pointer to compare function
+   
+   
+}
+
