b91a4e1e7adb1e566162e6b253957e5da7bc2cb9
Matt Halpern <Matt@Matts-MacBook-Pro.local>
2013-04-21 15:03:44 -0500

    flash storage bug fixes

--- embedded/hardware/src/file_system.c
+++ embedded/hardware/src/file_system.c
@@ -1,80 +1,74 @@
 
-#define SCHEDULE_SIZE 16384
-
+#include "file_system.h"
+#include "flash_prog.h"
+     
 #define SCHEDULE_REGISTRY_SIZE   256
-#define SCHEDULE_REGISTRY_ADDR   0x00008000
+#define SCHEDULE_REGISTRY_ADDR   0x00007C00
 #define EMPTY_SCHEDULE_ENTRY     0xFFFFFFFF
-#define SCHEDULE_BUFFER_A_ADDR   0x000084000
-#define SCHEDULE_BUFFER_B_ADDR   0x0000C400
+#define SCHEDULE_BUFFER_A_ADDR   0x00008000
+#define SCHEDULE_BUFFER_B_ADDR   0x0000C000
 
 #define CLOCK_FREQ_MHZ          6           // 6 MHz clock frequency
 
-void Flash_Init(unsigned char systemClockFreqMHz);
-int Flash_Write(unsigned long addr, unsigned long data);
-int Flash_ProgramArray(unsigned long *source, unsigned long addr, unsigned short count);
-int Flash_Erase(unsigned long addr);
 
-
-typedef struct {
-    unsigned long zone;
-    unsigned long start_time;
-    unsigned long end_time;
-    unsigned long not_completed;
-} schedule_entry_t;
-
+/* TODO - MH - Discuss - too much space 
 typedef union {
     unsigned long words[64][256]; // broken up into sectors...
     schedule_entry_t entries[341];
 } schedule_t;
+*/
 
-unsigned long *Schedule_Registry; 
+unsigned long *ScheduleRegistry; 
+unsigned long ScheduleRegistrySize;
 
 unsigned long *VALID_SCHEDULE;
 unsigned long *INVALID_SCHEDULE;
-unsigned long ScheduleIndex = 0;
 
-unsigned long *Schedule, *TempSchedule, *NextEmptyScheduleEntry;
+unsigned long ScheduleSize, TempScheduleSize;
+
+
+schedule_entry_t *Schedule, *TempSchedule;
 
 // TODO schedule_entry_t Schedule_Get_Entry(...)
 
 void Schedule_Init(void)
 {
-    unsigned long i = 0;
-
     Flash_Init(CLOCK_FREQ_MHZ);
 
-    Schedule_Registry = (unsigned long *) SCHEDULE_REGISTRY_ADDR;
+    ScheduleRegistry = (unsigned long *) SCHEDULE_REGISTRY_ADDR;
+	  ScheduleRegistrySize = 0;
 
     // look for current schedule (if one exists!)
-    while((Schedule_Registry[i] == EMPTY_SCHEDULE_ENTRY) && (i < 256)) {
-        i++;
+    while((ScheduleRegistry[ScheduleRegistrySize] != EMPTY_SCHEDULE_ENTRY) && (ScheduleRegistrySize < 256)) {
+        ScheduleRegistrySize++;
     }
 
     // check if first time loading a schedule
-    if(i == SCHEDULE_REGISTRY_SIZE    ) { 
-        i = 0;
+    if((ScheduleRegistrySize % SCHEDULE_REGISTRY_SIZE) == 0) { 
         Flash_Erase(SCHEDULE_REGISTRY_ADDR);
         Flash_Erase(SCHEDULE_BUFFER_A_ADDR);
         Flash_Write(SCHEDULE_REGISTRY_ADDR, SCHEDULE_BUFFER_A_ADDR);
-        Schedule = (unsigned long *) SCHEDULE_BUFFER_A_ADDR;
-        ScheduleIndex = 0;
+        Schedule = (schedule_entry_t *) SCHEDULE_BUFFER_A_ADDR;
+			  TempSchedule = (schedule_entry_t *) SCHEDULE_BUFFER_B_ADDR;
+        ScheduleSize = 0;
+			  TempScheduleSize = 0;
+			  ScheduleRegistrySize = 1;
     } else {
-        Schedule = (unsigned long *) Schedule_Registry[i];
-        ScheduleIndex = 0;
-        if(Schedule == (unsigned long*) SCHEDULE_BUFFER_A_ADDR)  {
-            TempSchedule = (unsigned long *) SCHEDULE_BUFFER_B_ADDR;
-            NextEmptyScheduleEntry = TempSchedule;
+        Schedule = (schedule_entry_t *) ScheduleRegistry[ScheduleRegistrySize-1];
+        ScheduleSize = TempScheduleSize;
+			  TempScheduleSize = 0;
+        if(Schedule == (schedule_entry_t *) SCHEDULE_BUFFER_A_ADDR)  {
+            TempSchedule = (schedule_entry_t *) SCHEDULE_BUFFER_B_ADDR;
         } 
         else {  
-            TempSchedule = (unsigned long *) SCHEDULE_BUFFER_A_ADDR;
-            NextEmptyScheduleEntry = TempSchedule;
+            TempSchedule = (schedule_entry_t *) SCHEDULE_BUFFER_A_ADDR;
         }
     }
 }
 
 int Schedule_Enter(schedule_entry_t *entry_ptr)
 {
-    unsigned long next_entry_addr = (unsigned long) NextEmptyScheduleEntry;
+    unsigned long next_entry_addr = (unsigned long) &TempSchedule[TempScheduleSize];
     // make sure sure schedule buffer is not full
     // TODO make -1 an error enum?
 
@@ -86,27 +80,26 @@ int Schedule_Enter(schedule_entry_t *entry_ptr)
     if(next_entry_addr % 0x400 == 0) // TODO FLASH_SECTOR_SIZE
         Flash_Erase(next_entry_addr);
 
-    entry_ptr->not_completed = 0xFFFFFFFF; // TODO
+    //entry_ptr->not_completed = 0xFFFFFFFF; // TODO
     Flash_ProgramArray((unsigned long *) entry_ptr, next_entry_addr, 4);
 
-    NextEmptyScheduleEntry = NextEmptyScheduleEntry + 4; 
+    TempScheduleSize++;
 
     return 0; // TODO SUCCESS
 }
 
 void Schedule_Refresh(void) 
 {
-    if(Schedule == (unsigned long *) SCHEDULE_BUFFER_A_ADDR)  {
-        Schedule = (unsigned long *) SCHEDULE_BUFFER_B_ADDR;
-        ScheduleIndex = 0;
-        TempSchedule = (unsigned long *) SCHEDULE_BUFFER_A_ADDR;
-        NextEmptyScheduleEntry = TempSchedule;
-    } 
-    else {  
-        Schedule = (unsigned long *) SCHEDULE_BUFFER_A_ADDR;
-        ScheduleIndex = 0;
-        TempSchedule = (unsigned long *) SCHEDULE_BUFFER_B_ADDR;
-        NextEmptyScheduleEntry = TempSchedule;
-
+    if(ScheduleRegistrySize == 256) {
+        Flash_Erase(SCHEDULE_REGISTRY_ADDR);
+			  ScheduleRegistrySize = 0;
     }
+		
+		Flash_Write((unsigned long) &ScheduleRegistry[ScheduleRegistrySize], (unsigned long) TempSchedule);
+		TempSchedule = Schedule;
+	  Schedule = (schedule_entry_t *) ScheduleRegistry[ScheduleRegistrySize];
+		ScheduleRegistrySize++;
+	  
+		ScheduleSize = TempScheduleSize;
+	  TempScheduleSize = 0;
 }
