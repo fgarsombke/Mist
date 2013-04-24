365b2dfc68aaa72ed89dce67091d6a8a25c12547
makilian <maakilian@gmail.com>
2013-04-24 05:07:49 -0500

    api/aWeather.py

--- watercalc/ForecastET.py
+++ watercalc/ForecastET.py
@@ -9,11 +9,13 @@ import urllib
 import urllib2
 import json
 import ctypes
+from ctypes import c_double
+from ctypes import c_ulonglong
 import os
 
 def getForecastForLocationAndDate(latitude, longitude, begin, end):
-    url = "http://0.0.0.0:8080/api/weather?latitude=%s&longitude=%s&beginTime=%s&endTime=%s" % (latitude, longitude, begin, end)
-
+    url = "http://quattrotap.com:8080/api/weather?latitude=%s&longitude=%s&begin=%s&end=%s" % (latitude, longitude, begin, end)
+    print url
     req = urllib2.Request(url)
     try:
         response = json.loads(urllib2.urlopen(req).read())
@@ -27,15 +29,20 @@ def getForecastForLocationAndDate(latitude, longitude, begin, end):
 
 def forecastETo(latitude, longitude, begin, end):
     wdObject = getForecastForLocationAndDate(latitude, longitude, begin, end)
-    
-    #USE ctypes to turn this WeatherData object into an ETo value: Graczyk's extern C function.
-    #ETCalc = ctypeslib.load_library('ETCalc.so', '.')
+    wd = wdObject[0]
+    print wd['MaxRH']
+    #CALL C++ ET CODE
+    ET = ctypes.cdll.LoadLibrary('/Users/makilian/Mist/lib/libETCalc.so')
+    ETCalc = ET.CalculateET_0
+    ETCalc.argtypes = [c_double, c_double, c_ulonglong, c_ulonglong, c_double, c_double, c_double, c_double, c_double, c_double, c_double, c_double, c_double, c_double]
+    eto = ETCalc(latitude, longitude, begin, end, 30.0, 40.0, 35.0, .2, .8, 32.0, 39.0, 100.0, 25.0, .7)
 
-    #then return ETo
-    return 7.0
+    print eto
+
+    return eto
 
 def main():
-    forecastETo(30, -100, "1366768754", "1366779536")
+    forecastETo(40, -90, "1366768754", "1366779536")
 
 if __name__ == '__main__':
     main()  
