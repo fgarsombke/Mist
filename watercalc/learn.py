8c9cae40cd3568cc7786fd10b7cf3cdff8429a83
Ubuntu <ubuntu@ip-10-112-83-154.ec2.internal>
2013-04-30 16:32:42 -0500

    changes

--- watercalc/learn.py
+++ watercalc/learn.py
@@ -39,14 +39,14 @@ def main():
     #RANDOM DATA FOR TESTING
     deviceID = 1
     zone = 1
-    simulatedTime = 13674483320 
+    simulatedTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    simulatedTime = datetime.datetime.strptime(simulatedTime, "%Y-%m-%d %H:%M:%S")
+    print simulatedTime
     doLearning(deviceID, zone, simulatedTime)
 
 def doLearning(deviceID, zone, simulatedTime):
     #determine state
     numIterations = getNumIterations(deviceID, zone)
-    simulatedTime = time.mktime(simulatedTime.timetuple())
-    print simulatedTime
     if numIterations > 0:
         event = getMostRecentIrrigationEventForDevice(deviceID, zone, simulatedTime)
         vectorID = event[5]
