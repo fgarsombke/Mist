cc3a405c66aa186e0843667300b93aa061abb9bb
makilian <maakilian@gmail.com>
2013-05-02 00:07:37 -0500

    added script to make learning simulations easier

---
+++ db/Delete.py
@@ -0,0 +1,22 @@
+import re
+import sys
+import MySQLdb
+import DBConfig
+
+def main():
+	sql = open("db/delete.sql").read()
+	sql = re.split('\n\s*\n', sql)	
+	print len(sql)
+
+	conf = DBConfig.DBConfig()
+	db = conf.connectToLocalConfigDatabase()
+	
+	for command in sql:
+		print command
+		cursor = db.cursor()	
+		cursor.execute(command)
+		cursor.close()
+
+
+if __name__ == '__main__':
+	main()
---
+++ db/delete.sql
@@ -0,0 +1,11 @@
+DELETE FROM learning;
+
+DELETE FROM devices;
+
+DELETE FROM queuedIrrigations;
+
+DELETE FROM vectors;
+
+ALTER TABLE learning AUTO_INCREMENT = 1;
+
+ALTER TABLE devices AUTO_INCREMENT = 1;
