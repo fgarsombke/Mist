d2026037afe8fe5d3e5819e73abe9f5329caa105
Michael Graczyk <michael@mgraczyk.com>
2013-05-05 20:59:16 -0500

    Fixed multiple f(c) evaluation

--- img_proc/bisect.m
+++ img_proc/bisect.m
@@ -2,16 +2,21 @@ function [ val ] = bisect( f, a, b, tol, nmax)
     val = NaN;
     for i = 1:nmax
         c = floor( (a + b)/2 );
-        if( ( f(c) == 0 ) || ( (b - a)/2 < tol ) )
-            val = f(c);
+        
+        f_c = f(c);
+        if( ( f_c == 0 ) || ( (b - a)/2 < tol ) )
+            val = f_c;
             return;
         end
-        if( f(c)*f(a) > 0 )
+        
+        f_a = f(a);
+        if( f_c*f_a > 0 )
             a = c;
         else
             b = c;
         end
     end
-    disp('failed');
+    
+    error('Iterations exceeded nmax');
 end
 
